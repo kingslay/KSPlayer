4c44d27304ade677f0e9f3a6d423320b62d37c63
kintanwang <kintanwang@tencent.com>
2019-04-09 17:27:52 +0800

    更新playground

--- Demo/MyPlayground.playground/Contents.swift
+++ Demo/MyPlayground.playground/Contents.swift
@@ -4,28 +4,26 @@ import KSPlayer
 import PlaygroundSupport
 import UIKit
 class ViewController: UIViewController {
-    var player = VideoPlayerController()
+    var playerView = IOSVideoPlayerView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(player.view)
-        player.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(playerView)
+        playerView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            player.view.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor),
-            player.view.leftAnchor.constraint(equalTo: view.leftAnchor),
-            player.view.rightAnchor.constraint(equalTo: view.rightAnchor),
-            player.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            playerView.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor),
+            playerView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            playerView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            playerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
-        view.layoutIfNeeded()
         var url = URL(string: "http://wvideo./spriteapp.cn/video/2016/0328/56f8ec01d9bfe_wpd.mp4")!
         url = URL(fileURLWithPath: Bundle.main.path(forResource: "567082ac3ae39699f68de4fd2b7444b1e045515a", ofType: "MP4")!)
-        player.set(url: url)
-        player.play()
+        playerView.set(url: url)
+        playerView.play()
     }
 }
 
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = ViewController()
-
 // let url = URL(fileURLWithPath: Bundle.main.path(forResource: "567082ac3ae39699f68de4fd2b7444b1e045515a", ofType: "MP4")!)
 // let playerLayer = KSPlayerLayer()
 // playerLayer.set(url: url, options: nil)
