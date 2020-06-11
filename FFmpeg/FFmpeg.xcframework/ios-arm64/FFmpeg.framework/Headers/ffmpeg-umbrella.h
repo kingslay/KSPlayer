1da0c909290403d623e28957378be5c03b1081e5
kintan <kingslay@icloud.com>
2020-06-11 23:27:17 +0800

    remove  FFmpegError.h .c

--- FFmpeg/FFmpeg.xcframework/ios-arm64/FFmpeg.framework/Headers/ffmpeg-umbrella.h
+++ FFmpeg/FFmpeg.xcframework/ios-arm64/FFmpeg.framework/Headers/ffmpeg-umbrella.h
@@ -1,6 +1,4 @@
-#import <Foundation/Foundation.h>
-#import <VideoToolbox/VideoToolbox.h>
-#import <AudioToolbox/AudioToolbox.h>
+#import <stdbool.h>
 #import "libavutil/samplefmt.h"
 #import "libavcodec/avcodec.h"
 #import "libavutil/pixdesc.h"
@@ -16,4 +14,20 @@
 #import "libavutil/avutil.h"
 #import "libswscale/swscale.h"
 #import "libswresample/swresample.h"
+
+int AVERROR_CONVERT(int err) {
+    return AVERROR(err);
+}
+
+bool IS_AVERROR_EOF(int err) {
+    return err == AVERROR_EOF;
+}
+
+bool IS_AVERROR_INVALIDDATA(int err) {
+    return err == AVERROR_INVALIDDATA;
+}
+
+bool AVFILTER_EOF(int ret) {
+    return ret == AVERROR(EAGAIN) || IS_AVERROR_EOF(ret);
+}
 
