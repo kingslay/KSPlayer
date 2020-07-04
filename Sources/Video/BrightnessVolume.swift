968b43a28d4f3d169ebfad1ade4567d72740d3d7
kintan <kingslay@icloud.com>
2020-07-04 17:26:33 +0800

    update rotate value

--- Sources/Video/BrightnessVolume.swift
+++ Sources/Video/BrightnessVolume.swift
@@ -177,7 +177,7 @@ private final class ProgressView: UIView {
         progressView.progressTintColor = UIColor.white
         progressView.trackTintColor = UIColor.white.withAlphaComponent(0.5)
         progressView.translatesAutoresizingMaskIntoConstraints = false
-        progressView.centerRotate(byDegrees: -.pi / 2)
+        progressView.centerRotate(byDegrees: -90)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             progressView.widthAnchor.constraint(equalToConstant: 115),
