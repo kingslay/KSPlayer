1acf90c7779110ff0249b2a76d3866a877e9622f
kintan <kingslay@icloud.com>
2021-03-03 10:13:58 +0800

    use audioUnitForTimePitch

--- Demo/demo-iOS/demo-iOS/MasterViewController.swift
+++ Demo/demo-iOS/demo-iOS/MasterViewController.swift
@@ -68,8 +68,8 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
             objects.append(KSPlayerResource(url: url, options: KSOptions(), name: "m3u8视频"))
         }
 
-        if let url = URL(string: "http://aldirect.hls.huya.com/huyalive/30765679-2504742278-10757786168918540288-3049003128-10057-A-0-1_1200.m3u8") {
-            objects.append(KSPlayerResource(url: url, options: KSOptions(), name: "直播流"))
+        if let url = URL(string: "https://bitmovin-a.akamaihd.net/content/dataset/multi-codec/hevc/stream_fmp4.m3u8") {
+            objects.append(KSPlayerResource(url: url, options: KSOptions(), name: "fmp4"))
         }
 
         if let url = URL(string: "http://116.199.5.51:8114/00000000/hls/index.m3u8?Fsv_chan_hls_se_idx=188&FvSeid=1&Fsv_ctype=LIVES&Fsv_otype=1&Provider_id=&Pcontent_id=.m3u8") {
