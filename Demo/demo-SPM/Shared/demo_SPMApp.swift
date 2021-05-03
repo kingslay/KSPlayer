d5cb36c1d45ae989692dda591f910bdb41753f50
kintan <kingslay@icloud.com>
2021-05-03 20:45:49 +0800

    add spm demo

---
+++ Demo/demo-SPM/Shared/demo_SPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_SPMApp.swift
+//  Shared
+//
+//  Created by kintan on 2021/5/3.
+//
+
+import SwiftUI
+
+@main
+struct demo_SPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
