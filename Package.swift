80165fa9933a0957bc27ed186bce9279c42ac894
kintan <kingslay@icloud.com>
2021-05-05 21:00:22 +0800

    update readme

--- Package.swift
+++ Package.swift
@@ -22,12 +22,27 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         .target(
             name: "KSPlayer",
-            dependencies: ["FFmpeg", "Libcrypto", "Libssl"]
+            dependencies: ["FFmpeg", "Libcrypto", "Libssl"],
+            linkerSettings: [
+                .linkedFramework("AudioToolbox"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreImage"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreVideo"),
+                .linkedFramework("MetalKit"),
+                .linkedFramework("Security"),
+                .linkedFramework("VideoToolbox")
+            ]
         ),
         .target(
             name: "FFmpeg",
             dependencies: ["Libavcodec", "Libavformat", "Libavutil", "Libswresample", "Libswscale", "Libssl", "Libcrypto"],
-            linkerSettings: [.linkedLibrary("bz2"), .linkedLibrary("iconv"), .linkedLibrary("xml2"), .linkedLibrary("z")]
+            linkerSettings: [
+                .linkedLibrary("bz2"),
+                .linkedLibrary("iconv"),
+                .linkedLibrary("xml2"),
+                .linkedLibrary("z")
+            ]
         ),
         .target(
             name: "Script",
@@ -51,7 +66,7 @@ let package = Package(
             name: "Libavutil",
             path: "Sources/Libavutil.xcframework"
         ),
-          .binaryTarget(
+        .binaryTarget(
             name: "Libswresample",
             path: "Sources/Libswresample.xcframework"
         ),
