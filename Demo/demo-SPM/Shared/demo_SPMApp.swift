3da9b1d044a24f530617dd5d4032d22ad9e7de3e
kintan <kingslay@icloud.com>
2021-12-07 22:03:59 +0800

    swiftformat

--- Demo/demo-SPM/Shared/demo_SPMApp.swift
+++ Demo/demo-SPM/Shared/demo_SPMApp.swift
@@ -5,13 +5,14 @@
 //  Created by kintan on 2021/5/3.
 //
 
-import SwiftUI
 import KSPlayer
+import SwiftUI
 @main
 struct demo_SPMApp: App {
     init() {
         KSPlayerManager.firstPlayerType = KSMEPlayer.self
     }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
