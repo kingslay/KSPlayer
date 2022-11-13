36d4136c120d989f60b4c703ea7dafd2d4928097
kintan <kingslay@icloud.com>
2022-11-13 21:30:41 +0800

    Slider add isFocused

--- Sources/KSPlayer/SwiftUI/KSVideoPlayerView.swift
+++ Sources/KSPlayer/SwiftUI/KSVideoPlayerView.swift
@@ -47,7 +47,7 @@ public struct KSVideoPlayerView: View {
                     subtitleModel.text = nil
                 }
             }
-            .onStateChanged { _, state in
+            .onStateChanged { playerLayer, state in
                 if state == .readyToPlay {
                     if let track = player.coordinator.subtitleTracks.first, options.autoSelectEmbedSubtitle {
                         player.coordinator.selectedSubtitleTrack = track
@@ -55,9 +55,11 @@ public struct KSVideoPlayerView: View {
                 } else if state == .bufferFinished {
                     if isMaskShow {
                         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + KSOptions.animateDelayTimeInterval) {
-                            isMaskShow = false
+                            isMaskShow = playerLayer.state != .bufferFinished
                         }
                     }
+                } else {
+                    isMaskShow = true
                 }
             }
             #if canImport(UIKit)
@@ -97,7 +99,25 @@ public struct KSVideoPlayerView: View {
                 }
             }
             subtitleView
-            VideoControllerView().opacity(isMaskShow ? 1 : 0)
+            VideoControllerView()
+            #if os(macOS)
+                .onMoveCommand { direction in
+                    isMaskShow = true
+                    switch direction {
+                    case .left:
+                        player.coordinator.skip(interval: -15)
+                    case .right:
+                        player.coordinator.skip(interval: 15)
+                    case .up:
+                        player.coordinator.playerLayer?.player.playbackVolume += 1
+                    case .down:
+                        player.coordinator.playerLayer?.player.playbackVolume -= 1
+                    @unknown default:
+                        break
+                    }
+                }
+            #endif
+                .opacity(isMaskShow ? 1 : 0)
             // 设置opacity为0，还是会去更新View。所以只能这样了
             if isMaskShow {
                 VideoTimeShowView(model: $model)
@@ -233,26 +253,10 @@ struct VideoControllerView: View {
             VideoSettingView()
         }
         .foregroundColor(.white)
-        #if !os(iOS)
-            .onMoveCommand { direction in
-                switch direction {
-                case .left:
-                    config.skip(interval: -15)
-                case .right:
-                    config.skip(interval: 15)
-                case .up:
-                    config.playerLayer?.player.playbackVolume += 1
-                case .down:
-                    config.playerLayer?.player.playbackVolume -= 1
-                @unknown default:
-                    break
-                }
-            }
-        #endif
         #if os(tvOS)
-        .onPlayPauseCommand {
-            config.isPlay.toggle()
-        }
+            .onPlayPauseCommand {
+                config.isPlay.toggle()
+            }
         #endif
     }
 }
@@ -438,9 +442,11 @@ public struct AirPlayView: UIViewRepresentable {
 
 #if os(tvOS)
 import Combine
-public struct Slider: UIViewRepresentable {
+@available(tvOS 15.0, *)
+public struct Slider: View {
     private let process: Binding<Float>
     private let onEditingChanged: (Bool) -> Void
+    @FocusState private var isFocused: Bool
     init(value: Binding<Double>, in bounds: ClosedRange<Double> = 0 ... 1, onEditingChanged: @escaping (Bool) -> Void = { _ in }) {
         process = Binding {
             Float((value.wrappedValue - bounds.lowerBound) / (bounds.upperBound - bounds.lowerBound))
@@ -450,18 +456,36 @@ public struct Slider: UIViewRepresentable {
         self.onEditingChanged = onEditingChanged
     }
 
+    public var body: some View {
+        TVOSSlide(value: process, isFocused: _isFocused, onEditingChanged: onEditingChanged)
+            .focused($isFocused)
+    }
+}
+
+@available(tvOS 15.0, *)
+public struct TVOSSlide: UIViewRepresentable {
+    private let process: Binding<Float>
+    private let onEditingChanged: (Bool) -> Void
+    @FocusState private var isFocused: Bool
+    init(value: Binding<Float>, isFocused: FocusState<Bool>, onEditingChanged: @escaping (Bool) -> Void = { _ in }) {
+        process = value
+        self.onEditingChanged = onEditingChanged
+        _isFocused = isFocused
+    }
+
     public typealias UIViewType = TVSlide
     public func makeUIView(context _: Context) -> UIViewType {
         TVSlide(process: process, onEditingChanged: onEditingChanged)
     }
 
     public func updateUIView(_ view: UIViewType, context _: Context) {
+        view.processView.tintColor = isFocused ? .red : .white
         view.process = process
     }
 }
 
 public class TVSlide: UIControl {
-    private let processView = UIProgressView()
+    let processView = UIProgressView()
     private lazy var panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(actionPanGesture(sender:)))
     private var beganProgress = Float(0.0)
     private let onEditingChanged: (Bool) -> Void
@@ -473,11 +497,22 @@ public class TVSlide: UIControl {
         }
     }
 
+    private var preMoveDirection: UISwipeGestureRecognizer.Direction?
+    private var preMoveTime = CACurrentMediaTime()
+    private lazy var timer: Timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+        guard let self, let preMoveDirection = self.preMoveDirection, preMoveDirection == .left || preMoveDirection == .right, self.process.wrappedValue < 0.99, self.process.wrappedValue > 0.01 else {
+            return
+        }
+        self.onEditingChanged(true)
+        self.process.wrappedValue += Float(preMoveDirection == .right ? 0.01 : -0.01)
+    }
+
     public init(process: Binding<Float>, onEditingChanged: @escaping (Bool) -> Void) {
         self.process = process
         self.onEditingChanged = onEditingChanged
         super.init(frame: .zero)
         processView.translatesAutoresizingMaskIntoConstraints = false
+        processView.tintColor = .white
         addSubview(processView)
         NSLayoutConstraint.activate([
             processView.topAnchor.constraint(equalTo: topAnchor),
@@ -488,7 +523,18 @@ public class TVSlide: UIControl {
         addGestureRecognizer(panGestureRecognizer)
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(actionTapGesture(sender:)))
         addGestureRecognizer(tapGestureRecognizer)
-        processView.tintColor = .blue
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(swipeGestureAction(_:)))
+        swipeDown.direction = .down
+        addGestureRecognizer(swipeDown)
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipeGestureAction(_:)))
+        swipeLeft.direction = .left
+        addGestureRecognizer(swipeLeft)
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swipeGestureAction(_:)))
+        swipeRight.direction = .right
+        addGestureRecognizer(swipeRight)
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipeGestureAction(_:)))
+        swipeUp.direction = .up
+        addGestureRecognizer(swipeUp)
     }
 
     @available(*, unavailable)
@@ -498,7 +544,7 @@ public class TVSlide: UIControl {
 
     @objc private func actionTapGesture(sender _: UITapGestureRecognizer) {
         panGestureRecognizer.isEnabled.toggle()
-        processView.tintColor = panGestureRecognizer.isEnabled ? .blue : .white
+        processView.tintColor = panGestureRecognizer.isEnabled ? .blue : .red
     }
 
     @objc private func actionPanGesture(sender: UIPanGestureRecognizer) {
@@ -520,6 +566,90 @@ public class TVSlide: UIControl {
             process.wrappedValue = beganProgress
             onEditingChanged(false)
         @unknown default:
+            break
+        }
+    }
+
+    override open func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
+        guard let presse = presses.first else {
+            return
+        }
+        switch presse.type {
+        case .leftArrow:
+            onEditingChanged(true)
+            if preMoveDirection == .left, CACurrentMediaTime() - preMoveTime < 0.2 {
+                timer.fireDate = Date.distantPast
+                break
+            } else {
+                timer.fireDate = Date.distantFuture
+                process.wrappedValue -= Float(0.01)
+                onEditingChanged(false)
+            }
+            preMoveDirection = .left
+            preMoveTime = CACurrentMediaTime()
+        case .rightArrow:
+            onEditingChanged(true)
+            if preMoveDirection == .right, CACurrentMediaTime() - preMoveTime < 0.2 {
+                timer.fireDate = Date.distantPast
+                break
+            } else {
+                timer.fireDate = Date.distantFuture
+                process.wrappedValue += Float(0.01)
+                onEditingChanged(false)
+            }
+            preMoveDirection = .right
+            preMoveTime = CACurrentMediaTime()
+        case .upArrow:
+            preMoveDirection = .up
+            preMoveTime = CACurrentMediaTime()
+            timer.fireDate = Date.distantFuture
+            onEditingChanged(false)
+        case .downArrow:
+            preMoveDirection = .down
+            preMoveTime = CACurrentMediaTime()
+            timer.fireDate = Date.distantFuture
+            onEditingChanged(false)
+        default: super.pressesBegan(presses, with: event)
+        }
+    }
+
+    @objc fileprivate func swipeGestureAction(_ recognizer: UISwipeGestureRecognizer) {
+        switch recognizer.direction {
+        case .left:
+            onEditingChanged(true)
+            if preMoveDirection == .left, CACurrentMediaTime() - preMoveTime < 0.02 {
+                timer.fireDate = Date.distantPast
+                break
+            } else {
+                timer.fireDate = Date.distantFuture
+                process.wrappedValue -= Float(0.01)
+                onEditingChanged(false)
+            }
+            preMoveDirection = .left
+            preMoveTime = CACurrentMediaTime()
+        case .right:
+            onEditingChanged(true)
+            if preMoveDirection == .right, CACurrentMediaTime() - preMoveTime < 0.02 {
+                timer.fireDate = Date.distantPast
+                break
+            } else {
+                timer.fireDate = Date.distantFuture
+                process.wrappedValue += Float(0.01)
+                onEditingChanged(false)
+            }
+            preMoveDirection = .right
+            preMoveTime = CACurrentMediaTime()
+        case .up:
+            preMoveDirection = .up
+            preMoveTime = CACurrentMediaTime()
+            timer.fireDate = Date.distantFuture
+            onEditingChanged(false)
+        case .down:
+            preMoveDirection = .down
+            preMoveTime = CACurrentMediaTime()
+            timer.fireDate = Date.distantFuture
+            onEditingChanged(false)
+        default:
             break
         }
     }
