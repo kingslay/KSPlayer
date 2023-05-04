06d5785f84b886e02812b16af2a1c519d3dfd964
kintan <kingslay@icloud.com>
2023-05-04 16:09:15 +0800

    拆分类

---
+++ Sources/KSPlayer/SwiftUI/AirPlayView.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftUIView.swift
+//  KSPlayer
+//
+//  Created by kintan on 2023/5/4.
+//
+
+import AVKit
+import SwiftUI
+
+public struct AirPlayView: UIViewRepresentable {
+    #if canImport(UIKit)
+    public typealias UIViewType = AVRoutePickerView
+    public func makeUIView(context _: Context) -> UIViewType {
+        let routePickerView = AVRoutePickerView()
+        routePickerView.tintColor = .white
+        return routePickerView
+    }
+
+    public func updateUIView(_: UIViewType, context _: Context) {}
+    #else
+    public typealias NSViewType = AVRoutePickerView
+    public func makeNSView(context _: Context) -> NSViewType {
+        let routePickerView = AVRoutePickerView()
+        return routePickerView
+    }
+
+    public func updateNSView(_: NSViewType, context _: Context) {}
+    #endif
+}
