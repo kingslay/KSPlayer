c2314d4bc78a8db3dbfec05e4873933583e046a4
kintan <kingslay@icloud.com>
2023-06-17 23:22:22 +0800

    fix build

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "KSPlayer",
-            type: .dynamic,
+            // todo clang: warning: using sysroot for 'iPhoneSimulator' but targeting 'MacOSX' [-Wincompatible-sysroot]
+//            type: .dynamic,
             targets: ["KSPlayer"]
         ),
     ],
