e708b128539220010a3d314c11ec43e94a9105d3
kintan <kingslay@icloud.com>
2023-06-25 22:41:21 +0800

    add test.m3u

--- Demo/SwiftUI/Shared/MovieModel.swift
+++ Demo/SwiftUI/Shared/MovieModel.swift
@@ -103,47 +103,6 @@ var testObjects: [MovieModel] = {
             objects.append(MovieModel(url: url, options: options))
         }
     }
-
-    if let url = URL(string: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4") {
-        let options = MEOptions()
-        options.startPlayTime = 25
-        objects.append(MovieModel(url: url, options: options, name: "mp4视频"))
-    }
-
-    if let url = URL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8") {
-        let options = MEOptions()
-        #if os(macOS)
-        let moviesDirectory = try? FileManager.default.url(for: .moviesDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-        options.outputURL = moviesDirectory?.appendingPathComponent("recording.mp4")
-        #endif
-        objects.append(MovieModel(url: url, options: options, name: "m3u8视频"))
-    }
-
-    if let url = URL(string: "https://bitmovin-a.akamaihd.net/content/dataset/multi-codec/hevc/stream_fmp4.m3u8") {
-        let options = MEOptions()
-        objects.append(MovieModel(url: url, options: options, name: "fmp4"))
-    }
-
-    if let url = URL(string: "http://116.199.5.51:8114/00000000/hls/index.m3u8?Fsv_chan_hls_se_idx=188&FvSeid=1&Fsv_ctype=LIVES&Fsv_otype=1&Provider_id=&Pcontent_id=.m3u8") {
-        objects.append(MovieModel(url: url, options: MEOptions(), name: "tvb视频"))
-    }
-
-    if let url = URL(string: "http://dash.edgesuite.net/akamai/bbb_30fps/bbb_30fps.mpd") {
-        objects.append(MovieModel(url: url, options: MEOptions(), name: "dash视频"))
-    }
-    if let url = URL(string: "https://devstreaming-cdn.apple.com/videos/wwdc/2019/244gmopitz5ezs2kkq/244/hls_vod_mvp.m3u8") {
-        let options = MEOptions()
-        objects.append(MovieModel(url: url, options: options, name: "https视频"))
-    }
-
-    if let url = URL(string: "rtsp://rtsp.stream/pattern") {
-        let options = MEOptions()
-        objects.append(MovieModel(url: url, options: options, name: "rtsp video"))
-    }
-
-    if let url = URL(string: "https://github.com/qiudaomao/MPVColorIssue/raw/master/MPVColorIssue/resources/captain.marvel.2019.2160p.uhd.bluray.x265-terminal.sample.mkv") {
-        objects.append(MovieModel(url: url, options: MEOptions(), name: "HDR MKV"))
-    }
     return objects
 }()
 
