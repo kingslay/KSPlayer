f7847f3fd64f6c80cf26de74ca5b99eb885ee3ab
kintan <kingslay@icloud.com>
2023-09-22 23:10:36 +0800

    优化ass样式解析

--- Sources/KSPlayer/Subtitle/KSSubtitle.swift
+++ Sources/KSPlayer/Subtitle/KSSubtitle.swift
@@ -57,6 +57,40 @@ public struct TextPosition {
         }
         return edgeInsets
     }
+
+    public mutating func ass(alignment: String?) {
+        switch alignment {
+        case "1":
+            verticalAlign = .bottom
+            horizontalAlign = .leading
+        case "2":
+            verticalAlign = .bottom
+            horizontalAlign = .center
+        case "3":
+            verticalAlign = .bottom
+            horizontalAlign = .trailing
+        case "4":
+            verticalAlign = .center
+            horizontalAlign = .leading
+        case "5":
+            verticalAlign = .center
+            horizontalAlign = .center
+        case "6":
+            verticalAlign = .center
+            horizontalAlign = .trailing
+        case "7":
+            verticalAlign = .top
+            horizontalAlign = .leading
+        case "8":
+            verticalAlign = .top
+            horizontalAlign = .center
+        case "9":
+            verticalAlign = .top
+            horizontalAlign = .trailing
+        default:
+            break
+        }
+    }
 }
 
 extension SubtitlePart: Comparable {
