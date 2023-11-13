cd826ff2be354be3320c150e2288c43b1d52f5e5
kingslay <kingslay@icloud.com>
2023-11-13 09:38:53 +0800

    解决依赖问题

--- Package.swift
+++ Package.swift
@@ -49,6 +49,6 @@ if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
     ]
 } else {
     package.dependencies += [
-        .package(url: "https://github.com/kingslay/FFmpegKit.git", from: "6.1"),
+        .package(url: "https://github.com/kingslay/FFmpegKit.git", from: "6.1.0"),
     ]
 }
