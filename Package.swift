a74c98f3075ef7d0570ba64be612c72c6a8a5312
Tinuv-Dev <161407084+Tinuv-Dev@users.noreply.github.com>
2024-04-15 11:25:21 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -65,6 +65,6 @@ if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
     ]
 } else {
     package.dependencies += [
-        .package(url: "https://github.com/kingslay/FFmpegKit.git", from: "6.1.2"),
+        .package(url: "https://github.com/Tinuv-Dev/FFmpegKit.git", .branch("main")),
     ]
 }
