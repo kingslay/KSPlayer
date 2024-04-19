481f1327451005e44cadaed9490ccf97279783f1
Tinuv-Dev <161407084+Tinuv-Dev@users.noreply.github.com>
2024-04-19 08:22:22 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -65,6 +65,6 @@ if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
     ]
 } else {
     package.dependencies += [
-        .package(url: "https://github.com/Tinuv-Dev/FFmpegKit.git", .branch("main")),
+        .package(url: "https://github.com/Tinuv-Dev/FFmpegKit.git", .exact("6.1.3.wav"))
     ]
 }
