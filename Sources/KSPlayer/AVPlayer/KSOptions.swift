b96f145cc72ec70099c2079fb500245d8f5b9665
kintan <kingslay@icloud.com>
2024-04-22 22:10:22 +0800

    fix 图片字幕一直存在的问题

--- Sources/KSPlayer/AVPlayer/KSOptions.swift
+++ Sources/KSPlayer/AVPlayer/KSOptions.swift
@@ -116,8 +116,8 @@ open class KSOptions {
         formatContextOptions["reconnect"] = 1
         formatContextOptions["reconnect_streamed"] = 1
         // 这个是用来开启http的链接复用（keep-alive）。vlc默认是打开的，所以这边也默认打开。
-        //开启这个，百度网盘的视频链接无法播放
-        //formatContextOptions["multiple_requests"] = 1
+        // 开启这个，百度网盘的视频链接无法播放
+        // formatContextOptions["multiple_requests"] = 1
         // 下面是用来处理秒开的参数，有需要的自己打开。默认不开，不然在播放某些特殊的ts直播流会频繁卡顿。
 //        formatContextOptions["auto_convert"] = 0
 //        formatContextOptions["fps_probe_size"] = 3
